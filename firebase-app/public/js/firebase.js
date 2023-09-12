8013c2d6d1c8769dda8f11823fb15b3e5f057efc
Axel S. Toro Vega <axelsariel@gmail.com>
2023-09-12 17:03:40 -0400

    added firebase

---
+++ firebase-app/public/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBuygMmEKiSF_pvzbRZrl0EGWM5HhB6AXU",
+  authDomain: "ec463miniproject-c8a9f.firebaseapp.com",
+  projectId: "ec463miniproject-c8a9f",
+  storageBucket: "ec463miniproject-c8a9f.appspot.com",
+  messagingSenderId: "883850515705",
+  appId: "1:883850515705:web:56c226eee2bc2aa2e57e28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
