2fb245e7024fd888597e63c17d520fd414781fe1
Axel S. Toro Vega <axelsariel@gmail.com>
2023-09-20 01:57:19 -0400

    Refresh has to be fixed

--- firebase-app/public/js/firebase.js
+++ firebase-app/public/js/firebase.js
@@ -16,34 +16,34 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 
 const provider = new GoogleAuthProvider();
 
-import { getAuth, signInWithRedirect } from "firebase/auth";
+import { getAuth, signInWithRedirect } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 
 const auth = getAuth();
 signInWithRedirect(auth, provider);
 
-import { getRedirectResult } from "firebase/auth";
+import { getRedirectResult } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 
-getRedirectResult(auth)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access Google APIs.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
+// getRedirectResult(auth)
+//   .then((result) => {
+//     // This gives you a Google Access Token. You can use it to access Google APIs.
+//     const credential = GoogleAuthProvider.credentialFromResult(result);
+//     const token = credential.accessToken;
 
-    // The signed-in user info.
-    const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+//     // The signed-in user info.
+//     const user = result.user;
+//     // IdP data available using getAdditionalUserInfo(result)
+//     // ...
+//   }).catch((error) => {
+//     // Handle Errors here.
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // The email of the user's account used.
+//     const email = error.customData.email;
+//     // The AuthCredential type that was used.
+//     const credential = GoogleAuthProvider.credentialFromError(error);
+//     // ...
+//   });
