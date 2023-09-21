cd722da9ba2fe9209a2a1e0d6a7c63a637e743ed
Axel S. Toro Vega <axelsariel@gmail.com>
2023-09-21 15:48:40 -0400

    Fixed login

--- firebase-app/public/js/firebase.js
+++ firebase-app/public/js/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
-import { GoogleAuthProvider, getRedirectResult, getAuth, signInWithRedirect, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,42 +19,29 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
 
-let currentUser = null;
-onAuthStateChanged(auth, user => currentUser = user);
-
-const redirectResult = await getRedirectResult(auth).catch(() => null);
-
-if (!redirectResult && !currentUser) { signInWithRedirect(auth, provider); }
-
-// getRedirectResult(auth)
-//   .then((result) => {
-//     // This gives you a Google Access Token. You can use it to access Google APIs.
-//     const credential = GoogleAuthProvider.credentialFromResult(result);
-//     const token = credential.accessToken;
-
-//     // The signed-in user info.
-//     const user = result.user;
-//     // IdP data available using getAdditionalUserInfo(result)
-//     // ...
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.customData.email;
-//     // The AuthCredential type that was used.
-//     const credential = GoogleAuthProvider.credentialFromError(error);
-//     // ...
-//   });
-
-
-// signInWithRedirect(auth, provider);
-debugger;
-// onAuthStateChanged(auth, user => {
-//   if (user) {
-//     // User is signed in.
-//     console.log('user is logged in');
-//   } else {
-//     console.log('user is not logged in')
-//   }
-// })
+onAuthStateChanged(auth, function(user) {
+  if (user) {
+    console.log('User is logged in')
+    console.log(user);
+  } else {
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+  }
+})
